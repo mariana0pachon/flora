505a6c6884d8567e86fce530a70bc6f6787cef18
Mariana Pachon Puentes <mariana@mojotech.com>
2024-05-23 10:17:27 +0200

    mas codigo semilla

--- semilla/semilla.ino
+++ semilla/semilla.ino
@@ -1,23 +1,28 @@
 int pinSensorHumedad = A0;
 int pinReleRio = 8;
-// int pinMotorFlorSubeBaja = x;
+int motorPin1 = 4;
+int motorPin2 = 2;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(pinSensorHumedad, INPUT);
   pinMode(pinReleRio, OUTPUT);
-  // pinMode(pinMotorFlorSubeBaja, OUTPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
 }
 
 void loop() {
   if (analogRead(pinSensorHumedad) > 550) {
+    Serial.println("se planto la semilla");
 
     // enviar senal OSC audio
 
     digitalWrite(pinReleRio, HIGH);
 
-    // despues de un delay prender motor 1 flores suben y bajan
+    delay(1000);  // Ver si se necesita mas delay
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
 
     // despues de un delay, enviar senal OSC mapping para la siguiente instruccion
   }
